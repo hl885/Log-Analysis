af9e6f3feb9bc4dd3407c304418a4ebb10edd3bd
h l <hangli885@gmail.com>
2017-12-06 19:39:58 -0500

    update readme, remove 'create view', fix formating and sql query

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 # SQL reporting tool
-Source code for reporting results from a database. The code formulates sql statements to query a postgresql database and return appropriate data from the database. The returned data is then output to a text file called 'sql_reporting_result.txt' generated under the same directory where the code is run.
+Source code for reporting results from a database containing website log data. The code formulates sql statements to query the database and return result from the database that answers the following questions:
 
-# Usage
-To run the program, first create a view in the postgresql datebase in command line tool:
-```sh
-$ psql news
+1. What are the most popular three articles of all time ?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
 
-$ create view popular_article as select articles.title as title, count(log.path) as view_count from articles, log where articles.slug = substring (log.path from 10) group by articles.title order by view_count desc;
-```
+## Requirements
+- Python 3 (https://www.python.org/downloads/)
+- PostgreSQL (https://www.postgresql.org/download/)
+- psycopg2
 
-Then, install python (https://www.python.org/downloads/), run the following command in command line tool:
-
-```sh
-$ python sql_reporting_tool.py
-```
-A text file called 'sql_reporting_result.txt' is generated under the same directory where the code is run.
+## Usage
+To run the program, do the following steps:
+1. setup the initial database:
+    ```sh
+    $ createdb news
+    ```
+2. import the data (download from https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip and unzip to the program directory)
+    ```sh
+    $ psql -d news -f newsdata.sql
+    ```
+3. create a view in the datebase:
+    ```sh
+    $ psql news
+    ```
+    ```sql
+    create view popular_article as
+    select articles.title as title, count(log.path) as view_count
+    from articles, log
+    where '/article/' || articles.slug = log.path
+    group by articles.title
+    order by view_count desc;
+    ```
+4. run the program
+    ```sh
+    $ ./sql_reporting_tool.py
+    ```
+After the program completes, a text file called 'sql_reporting_result.txt' containing the results is generated under the same directory where the code is run.
--- sql_reporting_tool.py
+++ sql_reporting_tool.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python 3
+#!/usr/bin/env python3
 import psycopg2
 
 
@@ -8,25 +8,13 @@ def main():
     sql_connect = psycopg2.connect("dbname=news")
     sql_cursor = sql_connect.cursor()
 
-    # Create view to find popular articles
-    try:
-        sql_cursor.execute(
-            """create view popular_article as
-            select articles.title as title, count(log.path) as view_count
-            from articles, log
-            where articles.slug = substring(log.path from 10)
-            group by articles.title
-            order by view_count desc;""")
-    except psycopg2.ProgrammingError:
-        sql_connect.rollback()
-
     # Select top 3 popular articles and write output to the file
     sql_cursor.execute("select * from popular_article limit 3;")
     section_title = "\r\nTop 3 popular articles: \r\n"
     print(section_title)
     report_file.write(section_title)
-    for row in sql_cursor.fetchall():
-        out_string = "\t\""+row[0].title()+"\" - "+str(row[1])+" views\r\n"
+    for title, views in sql_cursor.fetchall():
+        out_string = ('\t{0:<30}{1:>12} views\r\n'.format(title, views))
         print(out_string)
         report_file.write(out_string)
 
@@ -41,8 +29,8 @@ def main():
     section_title = "\r\nPoular article authors: \r\n"
     print(section_title)
     report_file.write(section_title)
-    for row in sql_cursor.fetchall():
-        out_string = "\t"+row[0]+" - "+str(row[1])+" views\r\n"
+    for title, views in sql_cursor.fetchall():
+        out_string = ('\t{0:<30}{1:>12} views\r\n'.format(title, views))
         print(out_string)
         report_file.write(out_string)
 
@@ -50,28 +38,28 @@ def main():
     # and write output to the file
     sql_cursor.execute(
         """select
-            tbl_200.trunc_time,
-            not_found_404::decimal/(not_found_404+ok_200) as error_rate
+            tbl_all.trunc_time,
+            not_found_404::decimal/total as error_rate
         from
             (select
-               date_trunc('day', time) as trunc_time,
-                count(status) as ok_200
-            from log where status = '200 OK'
-            group by trunc_time) as tbl_200,
+                date_trunc('day', time) as trunc_time,
+                count(status) as total
+            from log
+            group by trunc_time) as tbl_all,
             (select
                 date_trunc('day', time) as trunc_time,
                 count(status) as not_found_404
             from log where status = '404 NOT FOUND'
             group by trunc_time) as tbl_404
-        where tbl_200.trunc_time = tbl_404.trunc_time
-            and not_found_404::decimal/(not_found_404+ok_200) >= 0.01;""")
-    section_title = """
-        \r\nDays when more than 1% of requests lead to errors: \r\n"""
+        where tbl_all.trunc_time = tbl_404.trunc_time
+            and not_found_404::decimal/total >= 0.01;""")
+    section_title = (
+        "\r\nDays when more than 1% of requests lead to errors: \r\n")
     print(section_title)
     report_file.write(section_title)
-    for row in sql_cursor.fetchall():
-        out_string = "\t" + row[0].strftime("%B %d, %Y")
-        out_string += " - " + format(row[1], '.1%') + " errors\r\n"
+    for title, views in sql_cursor.fetchall():
+        out_string = (
+            '\t{0:%B %d, %Y}{1:>12.1%} errors\r\n'.format(title, views))
         print(out_string)
         report_file.write(out_string)
 
